a4c9d546e646ffcd47fb713bf863a704bff8aa86
Yurii <shadowjura82@gmail.com>
2023-07-30 21:52:08 +0200

    Initial commit

---
+++ src/main/java/com/example/trash_new/TrashNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.trash_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashNewApplication.class, args);
+    }
+
+}
